fe7f05ce1bf2eb826b127e1e20000ba547db0030
Wan Ab Muadz Adzim <muadz.nagasolo@gmail.com>
2020-12-11 07:01:45 +0000

    updated; to function

--- main.py
+++ main.py
@@ -1,23 +1,44 @@
 from PIL import Image, ImageDraw, ImageFont
+import os
 
-#Opening Image
-img = Image.open(r'test_imgs/APBJ2956.jpg') 
+proc_dir = r'test_imgs_wmarked'
+unproc_dir = r'test_imgs'
 
-#Creating draw object
-draw = ImageDraw.Draw(img) 
+proc_dir_list = os.listdir(proc_dir)
+unproc_dir_list = os.listdir(unproc_dir)
 
-#Creating text and font object
-text = "MataHelang"
-font = ImageFont.truetype('arial.ttf', 82)
+# List objects in specified dir
+def check_wmarked(proc_dir_list, unproc_dir_list):
+    return False if unproc_dir_list == proc_dir_list else True
 
-#Positioning Text
-textwidth, textheight = draw.textsize(text, font)
-width, height = img.size 
-x=width/2-textwidth/2
-y=height-textheight-300
+def batch_processing(proc_dir, unproc_dir, proc_dir_list, unproc_dir_list):
+    
+    #Creating text and font object
+    text = "MataHelang"
+    font = ImageFont.truetype('arial.ttf', 82)
+    
+    if check_wmarked(proc_dir_list, unproc_dir_list) == True:
+        for img in unproc_dir_list:
+            #Opening Image
+            img = Image.open(unproc_dir+'/'+img)
+            
+            #Creating draw object
+            draw = ImageDraw.Draw(img)
+    
+            #Positioning Text
+            textwidth, textheight = draw.textsize(text, font)
+            width, height = img.size 
+            x=width/2-textwidth/2
+            y=height-textheight-300
 
-#Applying text on image via draw object
-draw.text((x, y), text, font=font) 
+            #Applying text on image via draw object
+            draw.text((x, y), text, font=font)
 
-#Saving the new image
-img.save(r'test_imgs/APBJ2956_watermarked.png')
+            #Saving the new image
+            print(f'\nWatermarked image\n')
+            img.save(proc_dir+'/'+img)
+    else:
+        print(f'\nImage already been watermarked\n')
+
+if __name__ == "__main__":
+    batch_processing(proc_dir, unproc_dir, proc_dir_list, unproc_dir_list)
