3e468e763a70937ea0b5fe455529b39cde53b031
Wan Ab Muadz Adzim <muadz.nagasolo@gmail.com>
2020-12-12 12:29:32 +0000

    fixed; bugs regarding saving watermarked images

--- main.py
+++ main.py
@@ -20,14 +20,14 @@ def batch_processing(proc_dir, unproc_dir, proc_dir_list, unproc_dir_list):
     if check_wmarked(proc_dir_list, unproc_dir_list) == True:
         for img in unproc_dir_list:
             #Opening Image
-            img = Image.open(unproc_dir+'/'+img)
+            img_proc = Image.open(unproc_dir+'/'+img)
             
             #Creating draw object
-            draw = ImageDraw.Draw(img)
+            draw = ImageDraw.Draw(img_proc)
     
             #Positioning Text
             textwidth, textheight = draw.textsize(text, font)
-            width, height = img.size 
+            width, height = img_proc.size 
             x=width/2-textwidth/2
             y=height-textheight-300
 
@@ -35,10 +35,10 @@ def batch_processing(proc_dir, unproc_dir, proc_dir_list, unproc_dir_list):
             draw.text((x, y), text, font=font)
 
             #Saving the new image
-            print(f'\nWatermarked image\n')
-            img.save(proc_dir+'/'+img)
+            print(f'\nWatermarked image {img}\n')
+            img_proc.save(proc_dir+'/'+f'{img}'+'.png')
     else:
-        print(f'\nImage already been watermarked\n')
+        print(f'\nImage {img} already been watermarked\n')
 
 if __name__ == "__main__":
     batch_processing(proc_dir, unproc_dir, proc_dir_list, unproc_dir_list)
