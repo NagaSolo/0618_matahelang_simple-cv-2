ddbfdcb350369390a1ba8c7814b7aae3994f3c7d
Wan Ab Muadz Adzim <muadz.nagasolo@gmail.com>
2021-02-14 08:49:24 +0000

    added; users app to setting

--- jango/WMarkApp/settings.py
+++ jango/WMarkApp/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'postImage'
+    'postImage.apps.PostimageConfig',
+    'users.app.UsersConfig',
 ]
 
 MIDDLEWARE = [
