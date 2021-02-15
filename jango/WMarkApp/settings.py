ff960a9a39ad19712b5f4d0303503f0ff751cf6b
Wan Ab Muadz Adzim <muadz.nagasolo@gmail.com>
2021-02-15 13:50:05 +0000

    updated; url and setting at project

--- jango/WMarkApp/settings.py
+++ jango/WMarkApp/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'postImage.apps.PostimageConfig',
-    'users.app.UsersConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
