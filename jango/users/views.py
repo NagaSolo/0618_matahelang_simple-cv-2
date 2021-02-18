39a020eafd77dcb6ec992cc4f2fd2f0582e0f9d8
Wan Ab Muadz Adzim <muadz.nagasolo@gmail.com>
2021-02-18 09:16:44 +0000

    added; forms class, modified; view function

--- jango/users/views.py
+++ jango/users/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 
-# import user registration form from django.contrib
-from django.contrib.auth.forms import UserCreationForm
+# import user registration form from forms.py
+from .forms import UserRegisterForm
+
+from django.contrib import messages
 
 def register(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid:
+            form.save() # simpan valid form
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Akaun {username} berjaya dicipta')
+            return redirect('postImage-rumah')
+    else:
+        form = UserRegisterForm()
     return render(request, 'users/register.html', {'form' : form})
