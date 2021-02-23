90ece463ee377df45d1e1def77e3f76b30855da2
Wan Ab Muadz Adzim <muadz.nagasolo@gmail.com>
2021-02-23 14:25:51 +0000

    modified; PostImage model

--- jango/postImage/models.py
+++ jango/postImage/models.py
@@ -1,8 +1,13 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 class PostImage(models.Model):
     title = models.TextField()
+    content = models.TextField()
     cover = models.ImageField(upload_to='images/')
+    date_posted = models.DateTimeField(default=timezone.now)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
