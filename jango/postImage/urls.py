d7ac86c39428d23a1ff07845e6fb430767744b48
Wan Ab Muadz Adzim <muadz.nagasolo@gmail.com>
2021-02-24 08:47:05 +0000

    modified; codebase to include routing using class view

--- jango/postImage/urls.py
+++ jango/postImage/urls.py
@@ -2,7 +2,12 @@ from django.urls import path
 
 from . import views
 
+# class list view
+from .views import ImejListView, ImejDetailView
+
 urlpatterns = [
-    path('', views.rumah, name='postImage-rumah'),
+    # path('', views.rumah, name='postImage-rumah'),
+    path('', ImejListView.as_view(), name='postImage-rumah'),
+    path('imej/<int:pk>/', ImejDetailView.as_view(), name='postImage-detail'),
     path('tentang/', views.tentang, name='postImage-tentang'),
 ]
--- jango/postImage/views.py
+++ jango/postImage/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
 
 from .models import PostImage
+
+from django.views.generic import ListView, DetailView
+
+# Ini sudah tidak diguna
 def rumah(request):
     context = {
         'imej' : PostImage.objects.all()
     }
     return render(request, 'postimage/home.html', context)
+
+# gunapakai kelas ListView
+class ImejListView(ListView):
+    model = PostImage
+    template_name = 'postimage/home.html'
+    context_object_name = 'imej'
+    ordering = ['-date_posted'] # order imej in latest updated
+
+# gunapakai kelas DetailView untuk detail setiap post imej
+class ImejDetailView(DetailView):
+    model = PostImage
 
 def tentang(request):
     return render(request, 'postimage/about.html')
